f1872ed6264cf52ff1bde63bbb985f91b8c3b403
John Mitchell <john@johntellsall.com>
2023-09-16 14:30:47 -0700

    format

--- ex-chatgpt-cat-crm/cat_crm/cat_crm/models.py
+++ ex-chatgpt-cat-crm/cat_crm/cat_crm/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 class Cat(models.Model):
     name = models.CharField(max_length=255)
     breed = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
-    owner = models.ForeignKey('Owner', on_delete=models.CASCADE)
+    owner = models.ForeignKey("Owner", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -16,6 +19,8 @@ class Owner(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
 class Appointment(models.Model):
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
     date = models.DateField()
