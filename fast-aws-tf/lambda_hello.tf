7c704befec2f91540dda4209be8bc1ab60b809f0
John Mitchell <john@johntellsall.com>
2023-10-03 16:45:44 -0700

    full Lambda workflow works, including invoke

--- fast-aws-tf/lambda_hello.tf
+++ fast-aws-tf/lambda_hello.tf
@@ -1,20 +1,14 @@
 
 resource "aws_lambda_function" "lambda_hello" {
   function_name = "hello"
-  handler = "lambda_handler.lambda_handler" # This should match the handler function in your Python code
+  handler = "lambda_hello.lambda_handler"
   runtime = "python3.8"
   
-  # You can specify the execution role here or create it separately
-  # role = aws_iam_role.lambda_role.arn
-  
-  # Zip your Python code and any dependencies into a deployment package
   filename      = "lambda_hello.zip"
   source_code_hash = filebase64sha256("lambda_hello.zip")
   
-  # IAM Role for the Lambda function
   role = aws_iam_role.lambda_role.arn
   
-  # Set the timeout and memory size for your Lambda function
   timeout = 10
   memory_size = 128
 }
@@ -37,7 +31,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Attach necessary permissions to the Lambda role, for example, to access S3
+# FIXME: Attach necessary permissions to the Lambda role, for example, to access S3
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   name = "lambda_policy_attachment"
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess" # FIXME: Adjust the policy as needed
